d7d86f69426ad21e20b9e81a3022cd2aa5098154
LiuShuixiu <1067487478@qq.com>
2023-12-08 10:19:21 +0800

    01

---
+++ clean.bat
@@ -0,0 +1,12 @@
+rd /s /q assets
+rd /s /q blogs
+rd /s /q categories
+rd /s /q contain
+rd /s /q docs
+rd /s /q geology
+rd /s /q img
+rd /s /q ml_dl
+rd /s /q tag
+rd /s /q timeline
+del -q 404.html
+del -q index.html
---
+++ deploy.bat
@@ -0,0 +1,11 @@
+%放在上一级目录下执行，不得不吐槽一句这个反人类的反斜%
+
+
+xcopy /s /e /i /y public LiuShuixiu.github.io
+rd /s /q public
+
+
+cd LiuShuixiu.github.io
+call push
+
+pause
